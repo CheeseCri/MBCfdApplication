d5ed29086bc7bc364940eb04a98c5bc7a90ed10d
CheeseCri <yunghn3@gmail.com>
2020-11-25 09:44:03 +0900

    추첨 액티비티(미구현)

---
+++ app/src/main/java/com/example/sportgameprototype/LotteryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sportgameprototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LotteryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lottery)
+    }
+}
