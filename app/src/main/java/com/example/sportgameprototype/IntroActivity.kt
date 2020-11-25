ab6a1a5d59419463968b13d6c68f1f95f1b521c0
CheeseCri <yunghn3@gmail.com>
2020-11-25 14:19:45 +0900

    인트로화면 구현

--- app/src/main/java/com/example/sportgameprototype/IntroActivity.kt
+++ app/src/main/java/com/example/sportgameprototype/IntroActivity.kt
@@ -1,17 +1,23 @@
 package com.example.sportgameprototype
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import org.jetbrains.anko.startActivity
 
 class IntroActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT:Long = 2000 // 2 sec
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        val introTime : Long = 3000
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
-        Thread.sleep(introTime)
-        startActivity<MainMenuActivity>()
-        finish()
+        Handler().postDelayed({
+            startActivity(Intent(this,MainMenuActivity::class.java))
+            finish()  // close this activity
+        }, SPLASH_TIME_OUT)
     }
 }
