095aa794b4558e9d73ac5768474eede16f7826c2
CheeseCri <yunghn3@gmail.com>
2020-11-26 16:34:11 +0900

    추후 리사이클러뷰에 필요한 데이터 클래스 생성

---
+++ app/src/main/java/com/example/sportgameprototype/PlayerRecord.kt
@@ -0,0 +1,12 @@
+package com.example.sportgameprototype
+
+import java.time.LocalDate
+
+class PlayerRecord(
+        var SEASON_ID: Int,
+        var G_ID: String,
+        var AVG_RTM: Double,
+        var AVG_LD: Double,
+        var G_DT: String
+) {
+}
