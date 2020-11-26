8331d06269af63261fdd14af912b749f48ed2b77
CheeseCri <yunghn3@gmail.com>
2020-11-26 11:47:23 +0900

    :construction: 선수 상세정보 액티비티 작업 진행중

--- app/src/main/java/com/example/sportgameprototype/PlayerDetailActvity.kt
+++ app/src/main/java/com/example/sportgameprototype/PlayerDetailActvity.kt
@@ -5,11 +5,24 @@ import android.os.Bundle
 import android.widget.TextView
 
 class PlayerDetailActvity : AppCompatActivity() {
+    lateinit var tvPID : TextView
+    lateinit var tvPlayerName : TextView
+    lateinit var tvDebugSql : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_detail_activity)
+        /*
+         * 뷰 선언 부분
+         */
+        tvPID = findViewById<TextView>(R.id.tv_pid)
+        tvPlayerName = findViewById(R.id.tv_player_name)
+        tvDebugSql = findViewById(R.id.tv_debug_sql)
         val playerID = intent.getStringExtra("playerID")
-        val tvPID = findViewById<TextView>(R.id.tvPid)
+
+
+
         tvPID.text = playerID
+
+
     }
 }
