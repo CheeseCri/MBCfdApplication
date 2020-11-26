9fa3eab2c761928307c8c6d008a8e8f095441b66
CheeseCri <yunghn3@gmail.com>
2020-11-26 18:15:35 +0900

    리사이클러뷰 2개에 어댑터 및 레이아웃 매니저 적용, 화면에 표시 완료

--- app/src/main/java/com/example/sportgameprototype/PlayerDetailActvity.kt
+++ app/src/main/java/com/example/sportgameprototype/PlayerDetailActvity.kt
@@ -1,19 +1,21 @@
 package com.example.sportgameprototype
 
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import org.json.JSONArray
 import org.json.JSONObject
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 class PlayerDetailActvity : AppCompatActivity() {
     lateinit var tvPID : TextView
     lateinit var tvPlayerName : TextView
-    lateinit var tvDebugSql : TextView
+    lateinit var tvPlayerTeam : TextView
+    lateinit var rvSeasonAvg : RecyclerView
+    lateinit var rvRecentRecord : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_detail_activity)
@@ -22,27 +24,43 @@ class PlayerDetailActvity : AppCompatActivity() {
          */
         tvPID = findViewById<TextView>(R.id.tv_pid)
         tvPlayerName = findViewById(R.id.tv_player_name)
-        tvDebugSql = findViewById(R.id.tv_debug_sql)
+        tvPlayerTeam = findViewById(R.id.tv_player_team)
+        rvSeasonAvg = findViewById(R.id.rv_season_avg)
+        rvRecentRecord = findViewById(R.id.rv_recent_record)
 
         val playerID = intent.getStringExtra("playerID")
         val playerDetailInfo = HTTPtask().execute(getString(R.string.JSPURL), "player",playerID, "detail").get()
         val playerRecordInfo = HTTPtask().execute(getString(R.string.JSPURL), "player",playerID, "record").get()
         Log.d("USERLOG-detail", playerDetailInfo)
         Log.d("USERLOG-detail", playerRecordInfo)
-        val (avgRecord, recordSummary) = addRecord(JSONArray(playerRecordInfo))
+
+        val detailJsonArr = JSONArray(playerDetailInfo)
+        val detailJson : JSONObject = detailJsonArr[0] as JSONObject
+        tvPlayerName.text = detailJson.getString("P_NM")
+        tvPlayerTeam.text = detailJson.getString("T_ID")
         tvPID.text = playerID
-        tvDebugSql.text = playerRecordInfo
 
-        for (avg in avgRecord)
-            Log.d("USERLOG-DETAIL", "${avg.SEASON_ID},${avg.AVG_LD}${avg.AVG_RTM}")
-        for (record in recordSummary)
+        // seasonAvg : 시즌 평균
+        // recentRecord : 최근 5경기 기록
+        val (seasonAvg : ArrayList<AvgInfo>, recentRecord : ArrayList<PlayerRecord>) = addRecord(JSONArray(playerRecordInfo))
+
+        for (avg in seasonAvg)
+            Log.d("USERLOG-DETAIL", "${avg.SEASON_ID},${avg.AVG_LD},${avg.AVG_RTM}")
+        for (record in recentRecord)
             Log.d("USERLOG-DETAIL", "${record.G_DT},${record.AVG_LD},${record.AVG_RTM},")
-    }
-    private fun addRecord(jsonArray : JSONArray) : Pair<ArrayList<AvgInfo>, ArrayList<PlayerRecord>>{
-        var avgResult : ArrayList<AvgInfo> = ArrayList()
-        var recordSummary : ArrayList<PlayerRecord> = ArrayList()
-        var sumMap : MutableMap<Int, SumRecord> = mutableMapOf()
+        rvSeasonAvg.layoutManager = LinearLayoutManager(this)
+        rvSeasonAvg.adapter = SeasonAvgAdapter(this, seasonAvg)
+        rvRecentRecord.layoutManager = LinearLayoutManager(this)
+        rvRecentRecord.adapter = RecentRecordAdapter(this, recentRecord)
 
+
+
+    }
+
+    private fun addRecord(jsonArray : JSONArray) : Pair<ArrayList<AvgInfo>, ArrayList<PlayerRecord>>{
+        var seaonAvg : ArrayList<AvgInfo> = ArrayList()
+        var recentRecord : ArrayList<PlayerRecord> = ArrayList()
+        var sumMap : MutableMap<Int, SumRecord> = mutableMapOf()
 
         for (i in 0 until jsonArray.length()){
             val item = jsonArray.getJSONObject(i) as JSONObject
@@ -63,23 +81,16 @@ class PlayerDetailActvity : AppCompatActivity() {
                         AVG_LD = avgLd,
                         G_DT = item.getString("G_DT")
                 )
-                recordSummary.add(input)
+                recentRecord.add(input)
             }
         }
-
         for ((key, value) in sumMap){
-            avgResult.add(AvgInfo(key, value.sumLd / jsonArray.length(), value.sumRtm / jsonArray.length()))
+            seaonAvg.add(AvgInfo(key, value.sumLd / jsonArray.length(), value.sumRtm / jsonArray.length()))
         }
-
-        return Pair(avgResult, recordSummary)
+        return Pair(seaonAvg, recentRecord)
     }
 }
 
-class AvgInfo(
-        var SEASON_ID : Int,
-        var AVG_LD : Double,
-        var AVG_RTM : Double){
-}
 class SumRecord(
         var sumLd : Double,
         var sumRtm : Double)
