edf660b8c0c680bdd3ae4b6ee7aad957e35a2a05
CheeseCri <yunghn3@gmail.com>
2020-12-04 16:46:03 +0900

    가져온 데이터ID에 따라  String 적용시키는 함수 파일 작성

---
+++ app/src/main/java/com/example/sportgameprototype/DataTransform.kt
@@ -0,0 +1,33 @@
+package com.example.sportgameprototype
+
+import android.widget.ImageView
+
+fun posTransform(posNumber : Int) : String{
+    when(posNumber){
+        1 -> return "투수"
+        2 -> return "포수"
+        3 -> return "1루수"
+        4 -> return "2루수"
+        5 -> return "3루수"
+        6 -> return "유격수"
+        7 -> return "좌익수"
+        8 -> return "중견수"
+        9 -> return "우익수"
+        else -> return "?"
+    }
+}
+
+fun changeImage(view : ImageView, teamName : String){
+    when(teamName){
+        "HH" -> view.setImageResource(R.drawable.logo_hh)
+        "HT" -> view.setImageResource(R.drawable.logo_kia)
+        "KT" -> view.setImageResource(R.drawable.logo_kt)
+        "LG" -> view.setImageResource(R.drawable.logo_lg)
+        "LT" -> view.setImageResource(R.drawable.logo_lt)
+        "NC" -> view.setImageResource(R.drawable.logo_nc)
+        "OB" -> view.setImageResource(R.drawable.logo_ob)
+        "SK" -> view.setImageResource(R.drawable.logo_sk)
+        "SS" -> view.setImageResource(R.drawable.logo_ss)
+        "WO" -> view.setImageResource(R.drawable.logo_wo)
+    }
+}
