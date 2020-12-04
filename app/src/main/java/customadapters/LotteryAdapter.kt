493e46d68662be65b7371e2ab2dc9a18845bd539
CheeseCri <yunghn3@gmail.com>
2020-12-04 16:49:01 +0900

    추첨 선수 목록에서 선수 터치 시 선수 상세정보 보는 기능 추가

--- app/src/main/java/customadapters/LotteryAdapter.kt
+++ app/src/main/java/customadapters/LotteryAdapter.kt
@@ -1,16 +1,20 @@
-package com.example.sportgameprototype
+package customadapters
 
 import android.content.Context
-import android.text.Layout
+import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.CheckBox
 import android.widget.CompoundButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-
+import com.example.sportgameprototype.CheckBoxData
+import com.example.sportgameprototype.PlayerDetailActvity
+import com.example.sportgameprototype.PlayerInfo
+import com.example.sportgameprototype.R
 
 
 class LotteryAdapter(private val context : Context,
@@ -23,6 +27,14 @@ class LotteryAdapter(private val context : Context,
     lateinit var rtmPid : String
     lateinit var ldPid : String
 
+//    interface OnItemClickListener {
+//        fun onItemClick(view : View, position : Int)
+//    }
+//
+//    fun setOnItemClickListener(listener : OnItemClickListener){
+//
+//    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LotteryViewHolder {
         return LotteryViewHolder(LayoutInflater.from(context).inflate(
@@ -33,10 +45,10 @@ class LotteryAdapter(private val context : Context,
     }
 
     override fun onBindViewHolder(holder: LotteryViewHolder, position: Int) {
+        // 아이템 클릭 리스너 할
         // 텍스트 뷰 바인딩
         holder.tvLotteryItemPlayerName.text = lotteryPlayerList[position].playerName
-        holder.tvLotteryItemPlayerNum.text = lotteryPlayerList[position].backNumber
-
+        holder.tvLotteryItemPlayerNum.text = lotteryPlayerList[position].backNumber.toString()
         // No.7 Issue
         val cbListener = CompoundButton.OnCheckedChangeListener {  buttonView, isChecked ->
             when (buttonView.id) {
@@ -63,15 +75,20 @@ class LotteryAdapter(private val context : Context,
 
         holder.cbBestLd.setOnCheckedChangeListener(cbListener)
         holder.cbBestRtm.setOnCheckedChangeListener(cbListener)
+        holder.itemView.setOnClickListener {
+            val intent = Intent(it.context, PlayerDetailActvity::class.java)
+            intent.putExtra("playerID", lotteryPlayerList[position].playerId)
+            it.context.startActivity(intent)
+        }
 
         // 체크박스 리스트 업데이트
         if (position >= rtmCheckBoxList.size) {
-            Log.d("USERLOG-LOTERY-CB",": rtmCheckBoxList size : ${rtmCheckBoxList.size}")
+            Log.d("USERLOG-LOTTERY-CB",": rtmCheckBoxList size : ${rtmCheckBoxList.size}")
             rtmCheckBoxList.add(position, CheckBoxData(lotteryPlayerList[position].playerId, false))
             ldCheckBoxList.add(position, CheckBoxData(lotteryPlayerList[position].playerId, false))
         }
 
-        Log.d("USERLOG-LOTERY-CB",": rtmCheckBoxList[$position] : ${rtmCheckBoxList[position].checked}")
+        Log.d("USERLOG-LOTTERY-CB",": rtmCheckBoxList[$position] : ${rtmCheckBoxList[position].checked}")
         holder.cbBestLd.isChecked = ldCheckBoxList[position].checked
         holder.cbBestRtm.isChecked = rtmCheckBoxList[position].checked
     }
@@ -86,6 +103,8 @@ class LotteryAdapter(private val context : Context,
         val tvLotteryItemPlayerName: TextView = view.findViewById(R.id.tv_lottery_item_player_name)
         val tvLotteryItemPlayerNum: TextView = view.findViewById(R.id.tv_lottery_item_player_number)
     }
+
+
 
 }
 
