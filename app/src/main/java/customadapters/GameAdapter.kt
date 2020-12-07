79a384acc582ceae6b4dd9931daa13bfa2f0dc5c
CheeseCri <yunghn3@gmail.com>
2020-12-07 17:38:24 +0900

    S_ID를 실제 경기장 이름으로 변경하여 출력

--- app/src/main/java/customadapters/GameAdapter.kt
+++ app/src/main/java/customadapters/GameAdapter.kt
@@ -11,10 +11,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import com.example.sportgameprototype.GameInfo
-import com.example.sportgameprototype.LotteryActivity
-import com.example.sportgameprototype.R
-import com.example.sportgameprototype.changeImage
+import com.example.sportgameprototype.*
 
 class GameAdapter (private val context : Context,
                    private val gameList : ArrayList<GameInfo>
@@ -33,7 +30,7 @@ class GameAdapter (private val context : Context,
         val context = holder.itemView.context
         changeImage(holder.imgvHomeTeam, gameList[position].homeTeam)
         changeImage(holder.imgvAwayTeam, gameList[position].awayTeam)
-        holder.tvGameStation.text = gameList[position].stationName
+        holder.tvGameStation.text = changeStation(gameList[position].stationName)
         holder.tvGameTime.text = gameList[position].gameTime
         holder.btDoLottery.setOnClickListener {
             val intent = Intent(context, LotteryActivity::class.java)
