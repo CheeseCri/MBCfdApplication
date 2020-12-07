1a74498c0b9172db4d767865bba182d84188f4cf
CheeseCri <yunghn3@gmail.com>
2020-12-07 17:34:22 +0900

    년, 월일 나눠서 출력하도록 변경, 홈팀 어웨이팀 각각 출력하도록 변경

--- app/src/main/java/customadapters/UserRecordAdapter.kt
+++ app/src/main/java/customadapters/UserRecordAdapter.kt
@@ -1,6 +1,7 @@
 package customadapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sportgameprototype.R
 import com.example.sportgameprototype.UserRecordItem
+import com.example.sportgameprototype.changeTeam
 
 class UserRecordAdapter(private val context : Context,
                         private val userRecordItemList : ArrayList<UserRecordItem>)
@@ -22,10 +24,16 @@ class UserRecordAdapter(private val context : Context,
 
     override fun onBindViewHolder(holder: RecordViewHolder, position: Int) {
         val tmpItem : UserRecordItem = userRecordItemList[position]
-        holder.tvRecordGameDate.text = tmpItem.gameDate
-        holder.tvRecordHomevsaway.text = tmpItem.homeTeam + " VS " + tmpItem.awayTeam
+        holder.tvRecordHomeTeam.text = changeTeam(tmpItem.homeTeam)
+        holder.tvRecordAwayTeam.text = changeTeam(tmpItem.awayTeam)
+        // 연, 월일 나눠서 출력할 수 있도록 함.
+        Log.d("USERLOG-RecordAdapter", "${tmpItem.gameDate.toString()}")
+        holder.tvRecordGameYear.text = tmpItem.gameYear
+        holder.tvRecordGameMMDD.text = tmpItem.gameMMDD
+
         holder.tvRecordLdBp.text = tmpItem.bestLdPlayerName
         holder.tvRecordRtmBp.text = tmpItem.bestRtmPlayerName
+
         holder.tvRecordLdResult.text = if(tmpItem.isBestLd) "성공" else "실패"
         holder.tvRecordRtmResult.text = if(tmpItem.isBestRtm) "성공" else "실패"
     }
@@ -34,10 +42,16 @@ class UserRecordAdapter(private val context : Context,
         return userRecordItemList.size
     }
     class RecordViewHolder(view : View) : RecyclerView.ViewHolder(view){
-        val tvRecordGameDate : TextView = view.findViewById(R.id.tv_record_game_date)
-        val tvRecordHomevsaway : TextView = view.findViewById(R.id.tv_record_homevsaway)
+
+        val tvRecordHomeTeam : TextView = view.findViewById(R.id.tv_record_home_team)
+        val tvRecordAwayTeam : TextView = view.findViewById(R.id.tv_record_away_team)
+
+        val tvRecordGameYear : TextView = view.findViewById(R.id.tv_record_game_year)
+        val tvRecordGameMMDD : TextView = view.findViewById(R.id.tv_record_game_mmdd)
+
         val tvRecordRtmBp : TextView = view.findViewById(R.id.tv_record_rtm_bp)
         val tvRecordLdBp : TextView = view.findViewById(R.id.tv_record_ld_bp)
+
         val tvRecordRtmResult : TextView = view.findViewById(R.id.tv_record_rtm_result)
         val tvRecordLdResult : TextView = view.findViewById(R.id.tv_record_ld_result)
     }
