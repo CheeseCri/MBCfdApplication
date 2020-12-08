aa2479dc50634629ecab6425d7408368c52a3cae
CheeseCri <yunghn3@gmail.com>
2020-12-08 17:38:36 +0900

    리사이클러뷰 아이템 간격 넓히는 클래스 작성

---
+++ app/src/main/java/com/example/sportgameprototype/RecyclerDecoration.kt
@@ -0,0 +1,14 @@
+package com.example.sportgameprototype
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerDecoration(var divHeight : Int) : RecyclerView.ItemDecoration() {
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        super.getItemOffsets(outRect, view, parent, state)
+        if(parent.getChildAdapterPosition(view) != parent.adapter!!.itemCount - 1)
+            outRect.bottom = divHeight
+    }
+
+}
