8fc1bed63d618b7dd935d024687d9a6ed1c71ab6
CheeseCri <yunghn3@gmail.com>
2020-12-08 17:40:14 +0900

    Date 형식 property 추가

--- app/src/main/java/com/example/sportgameprototype/GameInfo.kt
+++ app/src/main/java/com/example/sportgameprototype/GameInfo.kt
@@ -1,12 +1,15 @@
 package com.example.sportgameprototype
 
+import java.util.*
+
 class GameInfo (
     var gameID : String,
     var season : String,
     var gameDate: String,
     var awayTeam: String,
-var homeTeam : String,
-var stationName : String,
-var gameTime : String) {
+    var homeTeam : String,
+    var stationName : String,
+    var gameTime : String,
+    var gDate : Date) {
     override fun toString() = "Game (gameID : $gameID)"
 }
