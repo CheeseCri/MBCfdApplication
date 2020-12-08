d055210cf97d8b38681c6e461960d2a2ff0a9f9e
CheeseCri <yunghn3@gmail.com>
2020-12-08 17:39:55 +0900

    시간 형식에 맞춰 출력하도록 코드 변경

--- app/src/main/java/customadapters/GameAdapter.kt
+++ app/src/main/java/customadapters/GameAdapter.kt
@@ -12,6 +12,7 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sportgameprototype.*
+import java.text.SimpleDateFormat
 
 class GameAdapter (private val context : Context,
                    private val gameList : ArrayList<GameInfo>
@@ -27,11 +28,15 @@ class GameAdapter (private val context : Context,
     }
 
     override fun onBindViewHolder(holder: gameViewHolder, position: Int){
+        val format = SimpleDateFormat("HH:mm")
         val context = holder.itemView.context
+        // 홈, 어웨이 팀 로고 변경
         changeImage(holder.imgvHomeTeam, gameList[position].homeTeam)
         changeImage(holder.imgvAwayTeam, gameList[position].awayTeam)
+
         holder.tvGameStation.text = changeStation(gameList[position].stationName)
-        holder.tvGameTime.text = gameList[position].gameTime
+
+        holder.tvGameTime.text = format.format(gameList[position].gDate)
         holder.btDoLottery.setOnClickListener {
             val intent = Intent(context, LotteryActivity::class.java)
             intent.putExtra("gameID", gameList[position].gameID)
